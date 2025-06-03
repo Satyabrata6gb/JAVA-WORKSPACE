e07a7175825af07802778803615228a070ab7d88
Satyabrata-Kar <satyabrata.kar@incture.com>
2025-06-03 23:12:36 +0530

    updated code base

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test {
+    // public static void main(String[] args) {
+        
+
+    //     String[] str = item.split(",");
+
+    //     String code = "";
+	// 	String lookupCode = "";
+
+    //     for (int k = 0; k < str.length; k++) {
+    //         if (null != str[k] && !"".equals(str[k]) && str[k].contains("=") && str[k].contains("code")) {
+    //             lookupCode = str[k].substring(str[k].indexOf('=') + 1, str[k].length());
+    //             code = code + lookupCode;
+    //             if (k != str.length - 1)
+    //                 code = code + "$$";
+    //         }
+    //     }
+
+    //     System.out.println(code);
+    //     System.out.println(lookupCode);
+    // }
+}
